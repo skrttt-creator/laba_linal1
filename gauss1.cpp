cc50b2cb9b86b13d77e643343fd3f15619a9a689
skrttt-creator <sasha061107@gmail.com>
2026-05-15 01:40:19 +0300

    Update and rename gauss_no_pivot.cpp to gauss1.cpp

--- gauss1.cpp
+++ gauss1.cpp
@@ -1,25 +1,24 @@
-#ifndef GAUSS_NO_PIVOT_H
-#define GAUSS_NO_PIVOT_H
+#ifndef GAUSS1_H
+#define GAUSS1_H
 
 #include <vector>
 #include <cmath>
 
 using namespace std;
 
-// Метод Гаусса без выбора ведущего элемента
-bool gaussNoPivot(vector<vector<double>> A, vector<double> b, vector<double>& x) {
-    int n = A.size();
+bool gauss1(vector<vector<double>> a, vector<double> b, vector<double>& x) {
+    int n = a.size();
     x.assign(n, 0.0);
     
     for (int k = 0; k < n; ++k) {
-        if (fabs(A[k][k]) < 1e-12) {
+        if (fabs(a[k][k]) < 1e-12) {
             return false;
         }
         
         for (int i = k + 1; i < n; ++i) {
-            double m = A[i][k] / A[k][k];
+            double m = a[i][k] / a[k][k];
             for (int j = k; j < n; ++j) {
-                A[i][j] -= m * A[k][j];
+                a[i][j] -= m * a[k][j];
             }
             b[i] -= m * b[k];
         }
@@ -28,9 +27,9 @@ bool gaussNoPivot(vector<vector<double>> A, vector<double> b, vector<double>& x)
     for (int i = n - 1; i >= 0; --i) {
         double sum = 0.0;
         for (int j = i + 1; j < n; ++j) {
-            sum += A[i][j] * x[j];
+            sum += a[i][j] * x[j];
         }
-        x[i] = (b[i] - sum) / A[i][i];
+        x[i] = (b[i] - sum) / a[i][i];
     }
     return true;
 }
