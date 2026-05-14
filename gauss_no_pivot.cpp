944a802e330d6585e9996b43e6ccfc988371cb08
skrttt-creator <sasha061107@gmail.com>
2026-05-15 01:35:32 +0300

    Create gauss_no_pivot.cpp

---
+++ gauss_no_pivot.cpp
@@ -0,0 +1,38 @@
+#ifndef GAUSS_NO_PIVOT_H
+#define GAUSS_NO_PIVOT_H
+
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+// Метод Гаусса без выбора ведущего элемента
+bool gaussNoPivot(vector<vector<double>> A, vector<double> b, vector<double>& x) {
+    int n = A.size();
+    x.assign(n, 0.0);
+    
+    for (int k = 0; k < n; ++k) {
+        if (fabs(A[k][k]) < 1e-12) {
+            return false;
+        }
+        
+        for (int i = k + 1; i < n; ++i) {
+            double m = A[i][k] / A[k][k];
+            for (int j = k; j < n; ++j) {
+                A[i][j] -= m * A[k][j];
+            }
+            b[i] -= m * b[k];
+        }
+    }
+    
+    for (int i = n - 1; i >= 0; --i) {
+        double sum = 0.0;
+        for (int j = i + 1; j < n; ++j) {
+            sum += A[i][j] * x[j];
+        }
+        x[i] = (b[i] - sum) / A[i][i];
+    }
+    return true;
+}
+
+#endif
