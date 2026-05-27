c04402d7627cfcf4b1f63e14e199ba9323bf8a9c
skrttt-creator <sasha061107@gmail.com>
2026-05-27 14:22:26 +0300

    Create experiments.hpp

---
+++ experiments.hpp
@@ -0,0 +1,13 @@
+#ifndef EXPERIMENTS_HPP
+#define EXPERIMENTS_HPP
+
+// Эксперимент 1: Замер времени решения одной системы
+void runExperiment1_SingleSystem();
+
+// Эксперимент 2: Экономия времени при множественных правых частях
+void runExperiment2_MultipleRHS();
+
+// Эксперимент 3: Тестирование точности на матрицах Гильберта
+void runExperiment3_HilbertAccuracy();
+
+#endif // EXPERIMENTS_HPP
